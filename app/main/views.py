eae94e640fb80ebcb9786424fc061ba44653b9e9
Muia23 <wilfred.muema@gmail.com>
2020-07-18 16:13:14 +0300

    add login_required decorator to redirect unauthorised users to login page

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,26 @@
 from flask import render_template,request,redirect,url_for
 from . import main
+from flask_login import login_required
 
 @main.route('/')
+def index():
+    '''
+    View root page function returning index page
+    '''
+    title = 'Soi | Where you express yourself'
+    return render_template('index.html', title = title)
+
+@main.route('/comment')
+@login_required
+def index():
+    '''
+    View root page function returning index page
+    '''
+    title = 'Soi | Where you express yourself'
+    return render_template('index.html', title = title)
+
+@main.route('/posting')
+@login_required
 def index():
     '''
     View root page function returning index page
