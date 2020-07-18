51ac9aafd4aad467c34f497b95a59bf2150c26a8
Muia23 <wilfred.muema@gmail.com>
2020-07-18 15:07:35 +0300

    create a column to store passwords

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class Quote:
     '''
@@ -11,8 +12,9 @@ class Quote:
 class User(db.Model):
     __tablename__='users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255))    
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
