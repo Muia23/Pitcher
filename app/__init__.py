c9bd77276600ead008a1c8cbbc14d8658738ae42
Muia23 <wilfred.muema@gmail.com>
2020-07-18 13:12:17 +0300

    initialize the app and create app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
 from flask import Flask
+from config import config_options
+
+def create_app(config_name):
+
+    #Initializing app
+    app = Flask(__name__)    
+
+    #create app config
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
+
+    return app
