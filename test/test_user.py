f436aa273cf2229f2c8bd7efdf91356475443db4
Muia23 <wilfred.muema@gmail.com>
2020-07-18 15:29:33 +0300

    add test to confirm AttributeError is raised and verify password is correct

--- test/test_user.py
+++ test/test_user.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models  import User
+from app.models import User
 
 class UserModelTest(unittest.TestCase):
 
@@ -8,3 +8,10 @@ class UserModelTest(unittest.TestCase):
 
     def test_password_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
+
+    def test_no_access_password(self):
+            with self.assertRaises(AttributeError):
+                self.new_user.password
+        
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('banana'))
